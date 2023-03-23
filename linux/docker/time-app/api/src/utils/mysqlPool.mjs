ccda1fd4c4b0157241df290651756d9540e07fca
Pavel Bespoludennov <bespoludennov@gmail.com>
2023-03-23 23:46:27 +0300

    docker study files from Bogdan Stashchuk

--- linux/docker/time-app/api/src/utils/mysqlPool.mjs
+++ linux/docker/time-app/api/src/utils/mysqlPool.mjs
@@ -1,12 +1,18 @@
 import mysql from 'mysql2'
 
+const MYSQL_HOST=process.env.MYSQL_HOST || 'mysql'
+const MYSQL_USER=process.env.MYSQL_USER || 'root'
+const MYSQL_PORT=process.env.MYSQL_PORT || '3306'
+const MYSQL_PASSWORD=process.env.MYSQL_PASSWORD || 'password'
+const MYSQL_DB=process.env.MYSQL_DB || 'admin'
+
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'mysql',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'time_db',
+  host: MYSQL_HOST,
+  port: MYSQL_USER,
+  user: MYSQL_PORT,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DB,
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
